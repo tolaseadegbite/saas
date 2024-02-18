6ddce7c98f3600cbef2d455462122e9209a8413b
tolaseadegbite <tolasekelvin@gmail.com>
2024-02-18 23:55:03 +0100

    Style login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :find_models
-
+    before_action :authenticate_user!
+    before_action :find_models, if: -> { user_signed_in? }
 
     private
     
