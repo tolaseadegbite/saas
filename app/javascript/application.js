16c50dc3600f3ff738f5fb2018fd4416e37a303f
tolaseadegbite <tolasekelvin@gmail.com>
2024-02-19 01:50:50 +0100

    Add jQuery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,22 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+import "./src/jquery"
+import "jquery-ui"
+
+$(".sidebar ul li").on('click', function () {
+    $(".sidebar ul li.active").removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.open-btn').on('click', function () {
+    $('.sidebar').addClass('active');
+
+});
+
+
+$('.close-btn').on('click', function () {
+    $('.sidebar').removeClass('active');
+
+})
