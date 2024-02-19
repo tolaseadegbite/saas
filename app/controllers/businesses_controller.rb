4f3fd0ea140fa9d16e6656c34cc7a26762edac35
tolaseadegbite <tolasekelvin@gmail.com>
2024-02-19 21:08:00 +0100

    Create products controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,58 +1,58 @@
 class BusinessesController < ApplicationController
-    before_action :authenticate_user!
-    before_action :find_business, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :find_business, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @businesses = current_user.businesses.ordered
-    end
+  def index
+    @businesses = current_user.businesses.ordered
+  end
 
-    def show
-      
-    end
+  def show
+    
+  end
 
-    def new
-      @business = Business.new
-    end
+  def new
+    @business = Business.new
+  end
 
-    def create
-      @business = current_user.businesses.build(business_params)
-      if @business.save
-        respond_to do |format|
-          format.html { redirect_to @business, notice: 'Your business has been successfully created' }
-        end
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-
-    def edit
-      
-    end
-
-    def update
-      if @business .update(business_params)
-        respond_to do |format|
-          format.html { redirect_to @business, notice: 'Your business has been successfully updated' }
-        end
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-
-    def destroy
-      @business.destroy
+  def create
+    @business = current_user.businesses.build(business_params)
+    if @business.save
       respond_to do |format|
-        format.html { redirect_to businesses_url, notice: 'Business deleted!' }
+        format.html { redirect_to @business, notice: 'Your business has been successfully created' }
       end
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    private
+  def edit
+    
+  end
 
-    def business_params
-      params.require(:business).permit(:name, :description, :user_id, :category_id)
+  def update
+    if @business .update(business_params)
+      respond_to do |format|
+        format.html { redirect_to @business, notice: 'Your business has been successfully updated' }
+      end
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def find_business
-      @business ||= current_user.businesses.find(params[:id])
+  def destroy
+    @business.destroy
+    respond_to do |format|
+      format.html { redirect_to businesses_url, notice: 'Business deleted!' }
     end
+  end
+
+  private
+
+  def business_params
+    params.require(:business).permit(:name, :description, :user_id, :category_id)
+  end
+
+  def find_business
+    @business ||= current_user.businesses.find(params[:id])
+  end
 end
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,64 @@
+class ProductsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :find_business
+    before_action :find_product, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @products = current_user.products.ordered
+    end
+
+    def show
+        
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = @business.products.build(product_params)
+        @product.user = current_user
+        if @product.save
+            respond_to do |format|
+                format.html { redirect_to @product, notice: 'Product successfully created' }
+            end
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        
+    end
+
+    def update
+        if @product .update(product_params)
+            respond_to do |format|
+                format.html { redirect_to @product, notice: 'Product successfully updated' }
+            end
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @product.destroy
+        respond_to do |format|
+            format.html { redirect_to products_url, notice: 'Product deleted!' }
+        end
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :description, :quantity, :unit_price)
+    end
+
+    def find_product
+      @product ||= current_user.products.find([:id])
+    end
+
+    def find_business
+        @business ||= current_user.businesses.find(params[:business_id])
+    end
+end
